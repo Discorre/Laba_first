e71ffa217341889d7245276ae3c129c00c76306d
Die Krummy <egorich.mih05@gmail.com>
2024-03-13 20:49:49 +0700

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -4,37 +4,71 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Выберите тип фигуры (rectangle / triangle):");
-        String choice = scanner.nextLine();
+        boolean continueLoop = true;
+        boolean continueLoop2 = true;
 
-        if (choice.equalsIgnoreCase("rectangle")) {
-            System.out.println("Введите ширину прямоугольника:");
-            double width = scanner.nextDouble();
-            System.out.println("Введите высоту прямоугольника:");
-            double height = scanner.nextDouble();
+        System.out.println("Выберите тип фигуры (прямоугольник / треугольник):");
 
-            Rectangle rectangle = new Rectangle(width, height);
-            System.out.println("Прямоугольник:");
-            System.out.println("Периметр: " + rectangle.getPerimeter());
-            System.out.println("Площадь: " + rectangle.getArea());
-            System.out.println("Длина диагонали: " + rectangle.getDiagonalLength());
-        } else if (choice.equalsIgnoreCase("triangle")) {
-            System.out.println("Введите длину первой стороны треугольника:");
-            double side1 = scanner.nextDouble();
-            System.out.println("Введите длину второй стороны треугольника:");
-            double side2 = scanner.nextDouble();
-            System.out.println("Введите длину третьей стороны треугольника:");
-            double side3 = scanner.nextDouble();
+        while (continueLoop) {
 
-            Triangle triangle = new Triangle(side1, side2, side3);
-            System.out.println("\nТреугольник:");
-            System.out.println("Периметр: " + triangle.getPerimeter());
-            System.out.println("Площадь: " + triangle.getArea());
-            System.out.println("Является ли треугольник равнобедренным: " + triangle.isIsosceles());
-        } else {
-            System.out.println("Неправильный выбор. Пожалуйста, введите 'rectangle' или 'triangle'.");
+            String choice = scanner.nextLine();
+
+            if (choice.equalsIgnoreCase("прямоугольник")) {
+                System.out.println("Введите ширину прямоугольника:");
+                double width = scanner.nextDouble();
+                System.out.println("Введите высоту прямоугольника:");
+                double height = scanner.nextDouble();
+
+                Rectangle rectangle = new Rectangle(width, height);
+                System.out.println("Выберите, что вы хотите получить (площадь / периметр / длина диагонали):");
+                String parameter = scanner.next();
+
+                switch (parameter.toLowerCase()) {
+                    case "площадь":
+                        System.out.println("Площадь прямоугольника: " + rectangle.getArea());
+                        break;
+                    case "периметр":
+                        System.out.println("Периметр прямоугольника: " + rectangle.getPerimeter());
+                        break;
+                    case "длина диагонали":
+                        System.out.println("Длина диагонали прямоугольника: " + rectangle.getDiagonalLength());
+                        break;
+                    default:
+                        System.out.println("Неверный выбор параметра.");
+                }
+            } else if (choice.equalsIgnoreCase("треугольник")) {
+                System.out.println("Введите длину первой стороны треугольника:");
+                double side1 = scanner.nextDouble();
+                System.out.println("Введите длину второй стороны треугольника:");
+                double side2 = scanner.nextDouble();
+                System.out.println("Введите длину третьей стороны треугольника:");
+                double side3 = scanner.nextDouble();
+
+                Triangle triangle = new Triangle(side1, side2, side3);
+                System.out.println("Выберите, что вы хотите получить (площадь / периметр / равнобедренность):");
+                String parameter = scanner.next();
+
+                switch (parameter.toLowerCase()) {
+                    case "площадь":
+                        System.out.println("Площадь треугольника: " + triangle.getArea());
+                        break;
+                    case "периметр":
+                        System.out.println("Периметр треугольника: " + triangle.getPerimeter());
+                        break;
+                    case "равнобедренность":
+                        System.out.println("Является ли треугольник равнобедренным: " + triangle.isIsosceles());
+                        break;
+                    default:
+                        System.out.println("Неверный выбор параметра.");
+                }
+
+            } else {
+                System.out.println("Неправильный выбор. Пожалуйста, введите 'прямоугольник' или 'треугольник'.");
+
+            }
+
+
         }
-
         scanner.close();
     }
 }
